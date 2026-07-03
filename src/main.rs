07541591b0a815d78e4b84434583e9041fdacd44
Ka-un <grasland.seb@gmail.com>
2026-07-03 10:52:05 +0200

    Ajout structure Task et affichage simple

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use mysql::*;
+mod task;
+
 
 fn main() {
+    let tache = task::Task {id: 1, title: "premiere tache".to_string(), done: false};
+    println!("{} {} {}", tache.id, tache.title, tache.done);
 }
