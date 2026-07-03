6c5bf6744667d1432efea64a9f0da0bd15e50289
Ka-un <grasland.seb@gmail.com>
2026-07-03 11:27:00 +0200

    Ajout d'une boucle pour modifier une tache

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,30 @@ mod task;
 
 
 fn main() {
+
+    //création des taches
     let mut taches: Vec<task::Task> = Vec::new();
     taches.push(task::Task {id: 1, title: "premier".to_string(), done: false});
     taches.push(task::Task {id: 2, title: "second".to_string(), done: false});
     taches.push(task::Task {id: 3, title: "troisieme".to_string(), done: false});
+
+    //affichages des taches
+    println!("-----");
     for i in &taches {
-        println!("{} {} {}", i.id, i.title, i.done)
+        println!("{} {} {}", i.id, i.title, i.done);
     }
-    //println!("{}", &taches[0]);
+    println!("-----");
+
+    //modification d'une tache
+    for i in &mut taches {
+        if i.id == 1 {
+            i.done = true;
+        }
+    }
+
+    println!("-----");
+    for i in &taches {
+        println!("{} {} {}", i.id, i.title, i.done);
+    }
+    println!("-----");
 }
