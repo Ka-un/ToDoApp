3604af99dc41d092649e18c6334e207ff3ffa776
Ka-un <grasland.seb@gmail.com>
2026-07-03 11:11:20 +0200

    Ajout d'une liste de tache et affichage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ mod task;
 
 
 fn main() {
-    let tache = task::Task {id: 1, title: "premiere tache".to_string(), done: false};
-    println!("{} {} {}", tache.id, tache.title, tache.done);
+    let mut taches: Vec<task::Task> = Vec::new();
+    taches.push(task::Task {id: 1, title: "premier".to_string(), done: false});
+    taches.push(task::Task {id: 2, title: "second".to_string(), done: false});
+    taches.push(task::Task {id: 3, title: "troisieme".to_string(), done: false});
+    for i in &taches {
+        println!("{} {} {}", i.id, i.title, i.done)
+    }
+    //println!("{}", &taches[0]);
 }
