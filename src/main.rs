5b20ed81032f53bbe879a3ac3aaddf1a99f46974
Ka-un <grasland.seb@gmail.com>
2026-07-03 14:11:08 +0200

    ajout d'une struct ToDoList pour identifier le vec<> associé && passage de la fonction add en imp de cette struct

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,31 @@ mod todo;
 fn main() {
 
     //création des taches
-    let mut taches: Vec<todo::Task> = Vec::new();
-    taches.push(todo::Task {id: 1, title: "premier".to_string(), done: false});
-    taches.push(todo::Task {id: 2, title: "second".to_string(), done: false});
-    taches.push(todo::Task {id: 3, title: "troisieme".to_string(), done: false});
+    let mut taches = todo::ToDoList {
+        tasks: Vec::new(),
+    };
+    todo::ToDoList::add(&mut taches, todo::Task {id: 1, title: "premier".to_string(), done: false});
+    todo::ToDoList::add(&mut taches, todo::Task {id: 2, title: "second".to_string(), done: false});
+    todo::ToDoList::add(&mut taches, todo::Task {id: 3, title: "troisieme".to_string(), done: false});
 
     //affichages des taches
     println!("-----");
-    for i in &taches {
+    for i in &taches.tasks {
         println!("{} {} {}", i.id, i.title, i.done);
     }
     println!("-----");
 
     //modification d'une tache
-    for i in &mut taches {
+    for i in &mut taches.tasks {
         if i.id == 1 {
             i.done = true;
         }
     }
 
     println!("-----");
-    for i in &taches {
+    for i in &taches.tasks {
         println!("{} {} {}", i.id, i.title, i.done);
     }
     println!("-----");
 }
 
-//fn add_todo(t: todo::Task) {
-//}
--- src/todo.rs
+++ src/todo.rs
@@ -3,3 +3,14 @@ pub struct Task {
     pub title: String,
     pub done: bool,
 }
+
+pub struct ToDoList {
+    pub tasks : Vec<Task>
+}
+
+//self représente la ToDoList | tasks l'élément de la struct
+impl ToDoList {
+    pub fn add(&mut self, t: Task) {
+        self.tasks.push(t);
+    }
+}
