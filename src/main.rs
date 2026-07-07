da601df9cf252261056591858e6533cf781a3741
Ka-un <grasland.seb@gmail.com>
2026-07-07 20:46:01 +0200

    Modification du menu affichage / Ajouter une tache / Demande à l'utilisateur de rentrer un id

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let mut taches = todo::ToDoList {
         tasks: Vec::new(),
     };
-    todo::ToDoList::print(&taches);
+    //todo::ToDoList::print(&taches);
     todo::ToDoList::add(&mut taches, todo::Task {id: 1, title: "premier".to_string(), done: false});
     // todo::ToDoList::add(&mut taches, todo::Task {id: 2, title: "second".to_string(), done: false});
     // todo::ToDoList::add(&mut taches, todo::Task {id: 3, title: "troisieme".to_string(), done: false});
@@ -19,10 +19,13 @@ fn main() {
     todo::ToDoList::remove(&mut taches, 1);
 
     loop {
-        ////clear le terminal à chaque boucle 
-        //print!("\x1B[2J\x1B[1;1H");
-
         println!("uuuuuuuuuu MENU uuuuuuuuuu");
+        println!("1) Afficher la ToDoList");
+        println!("2) Ajouter une tâche");
+        println!("3) Indiquer qu'une tâche est terminée");
+        println!("4) supprimer une tâche \n");
+
+        println!("uuuuuuuuuuuuuuuuuuuuuuuuuu");
 
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
@@ -32,8 +35,45 @@ fn main() {
             Err(_) => continue,
         };
 
-        println!{"vous avez choisis {}", input}
-    }
+        match input {
+            1 => todo::ToDoList::print(&taches),
+            2 => {
+                    let mut input_id: String = String::new();
+                    let mut input_title: String = String::new();
+                    let mut input_done: String = String::new();
 
+                    println!("Veuillez rentrer un id pour la nouvelle tâche");
+                    io::stdin().read_line(&mut input_id).unwrap();
+
+                    let input_id: u32 = match input_id.trim().parse() {
+                        Ok(n) => n,
+                        Err(_) => {
+                            println!("Ce n'est pas un nombre valide.");
+                            println!("Appuyez sur Entrée pour continuer...");
+
+                            let mut _buffer = String::new();
+                            io::stdin().read_line(&mut _buffer).unwrap();
+                            continue
+                        },
+                    };
+
+                    println!("Vous avez rentré le nombre {}", input_id);
+                    io::stdin().read_line(&mut input_title).unwrap();
+                    io::stdin().read_line(&mut input_done).unwrap();
+
+
+                    //todo::ToDoList::add(&mut taches, todo::Task {id: input_id, title: input_title, done: input_done});
+                },
+            _ => continue,
+        }
+
+        println!("Appuyez sur Entrée pour continuer...");
+
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+
+        ////clear le terminal à chaque boucle 
+        print!("\x1B[2J\x1B[1;1H");
+    }
 }
 
